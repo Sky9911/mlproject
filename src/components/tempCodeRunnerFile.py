1618b4b3492d1c9280cfa3f13fdf1814bae57303
Sky9911 <expressionwind71@gmail.com>
2025-11-06 18:08:53 +0530

    Model Trainer

---
+++ src/components/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+
+    # modeltrainer=ModelTrainer()
+    # print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
---
+++ src/utils.py
@@ -0,0 +1,92 @@
+import os
+import sys
+
+import numpy as np 
+import pandas as pd
+import dill
+import pickle
+from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+
+from src.exception import CustomException
+
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
+    try:
+        report = {}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+
+            #model.fit(X_train, y_train)  # Train model
+
+            y_train_pred = model.predict(X_train)
+
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+
+
+# from sklearn.model_selection import GridSearchCV
+# from sklearn.metrics import r2_score
+
+# def evaluate_models(X_train, y_train, X_test, y_test, models, param):
+#     try:
+#         report = {}
+#         for name, model in models.items():
+#             grid = param.get(name, {})  # handle missing grid
+
+#             if grid:  # tune if there is a grid
+#                 gs = GridSearchCV(
+#                     estimator=model,
+#                     param_grid=grid,
+#                     scoring="r2",
+#                     cv=3,
+#                     n_jobs=-1,
+#                     error_score="raise"
+#                 )
+#                 gs.fit(X_train, y_train)
+#                 best_model = gs.best_estimator_
+#             else:      # fit directly if no grid
+#                 model.fit(X_train, y_train)
+#                 best_model = model
+
+#             y_train_pred = best_model.predict(X_train)
+#             y_test_pred  = best_model.predict(X_test)
+
+#             train_r2 = r2_score(y_train, y_train_pred)
+#             test_r2  = r2_score(y_test,  y_test_pred)
+
+#             report[name] = test_r2  # you’re selecting by test score
+#         return report
+
+#     except Exception as e:
+#         raise CustomException(e, sys)
